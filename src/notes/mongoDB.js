5bb090fc9177e12a06ed8402dcdfef57b3972aea
linknn <crysnnelson@hotmail.com>
2025-09-26 15:27:51 -0700

    Update mongo notes

--- src/notes/mongoDB.js
+++ src/notes/mongoDB.js
@@ -6,7 +6,11 @@ Find by fields:
 
 Age > 30:
 { age: { $gt: 30 }}
+ // >= : $gte
+ // Find all ages greater than or equal to 30
 
 Age < 30:
-{ age: { $lt: 30 }}`;
+{ age: { $lt: 30 }}
+ // >= : $lte
+ // Find all ages less than or equal to 30`;
 export default mongoNotes;

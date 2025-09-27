5fb355d6040484ab2a65b2ef595fcd519bfd9364
linknn <crysnnelson@hotmail.com>
2025-09-27 01:06:07 -0700

    Update mongo and express notes

--- src/notes/express.js
+++ src/notes/express.js
@@ -4,6 +4,10 @@ router.get('/books', getBooks);
 router.post('/books', createBook);
 router.put('/books/:id', replaceBook);
 router.patch('/books/:id', updateBookInfo);
-router.delete('/books/:id', deleteBook);`;
+router.delete('/books/:id', deleteBook);
+
+imports are written as const and require the path
+    const express = require('express');
+`;
 
 export default expressNotes;

aaa1278504707e9637092763017fdf4fa4ca2e43
linknn <crysnnelson@hotmail.com>
2025-09-27 01:46:54 -0700

    Add bem notes page

---
+++ src/notes/bem.js
@@ -0,0 +1,12 @@
+const bemNotes = `block__element → double underscore separates an element of a block.
+
+block--modifier → double hyphen separates a modifier of a block.
+
+Use __ for elements
+
+Use -- for modifiers
+
+Hyphens - are just normal word separators
+`;
+
+export default bemNotes;

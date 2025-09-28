108d63d30db14b58daec8dfd7f63ef5c6c5b9fdd
linknn <crysnnelson@hotmail.com>
2025-09-28 02:00:03 -0700

    Start express server note

--- src/notes/express.js
+++ src/notes/express.js
@@ -8,6 +8,24 @@ router.delete('/books/:id', deleteBook);
 
 imports are written as const and require the path
     const express = require('express');
+
+    -------- Node.js -------
+
+to make app auto update each time you save install nodemon and run app with nodemon app.js
+
+on npm init -y flag answer yes to all initialization questions
+   
+Create an express project on port 3001:
+    const express = require("express");
+
+const app = express();
+
+const { PORT = 3001 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(Listening on port PORT); //PORT should be template literal
+});
+
 `;
 
 export default expressNotes;

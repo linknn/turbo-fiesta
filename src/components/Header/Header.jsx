058ac858d2830ffbaa6405a502c287700dcf09b0
linknn <crysnnelson@hotmail.com>
2025-09-29 22:20:14 -0700

    Style header and toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import ThemeToggle from "../ThemeToggle/ThemeToggle";
 export default function Header({ darkMode, setDarkMode, konamiActive }) {
   return (
     <header className="header">
-      <h1 className={`header__title ${konamiActive} ? "header__title--rainbow" : ""`}>
+      <h1 className={`header__title ${konamiActive ? "header__title--rainbow" : ""}`}>
         Cheat Sheet
       </h1>
       <ThemeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
